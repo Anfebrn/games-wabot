9a95c55199e8c637b7e012f2b283bf31ccb6b79d
Anfebrn <88342355+Anfebrn@users.noreply.github.com>
2021-08-05 00:26:47 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 let handler = function (m) {
   // this.fakeReply(m.chat, 'This is my owner', '6285924341386@s.whatsapp.net', 'Owner')
-  this.sendContact(m.chat, '6285349896689', 'anfebnBotz', m)
-  this.sendContact(m.chat, '6285924341386', 'Owner  AnfebnBotz:)', m)
+  this.sendContact(m.chat, '6287772343860', 'Owner anfebnBotz , m)
+  this.sendContact(m.chat, '6285924341386', 'Owner AnfebnBotz', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
